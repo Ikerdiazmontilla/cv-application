9355a5f4d2861815bff06d0966a3223734a75309
Ikerdiazmontilla <ikercorrevuela@gmail.com>
2024-05-08 12:28:36 -0500

    Make menu interactive

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,15 +1,26 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import Content from "./content/Content"
 import Menu from "./Menu"
 
 function App() {
-
+  const [ isContent, setIsContent ] = useState(true);
+  
+  const handleMenuClick = (event) => {
+    document.querySelectorAll('.menu-btn').forEach(btn => btn.classList.remove('active'))
+    if(event.currentTarget.id === 'content') {
+      setIsContent(true);
+    } else{
+      setIsContent(false);
+    }
+    event.currentTarget.className = 'menu-btn active'
+  };
   function handleChange() {}
 
   return (
     <div className="body">
-      <Menu/>
-      <Content handleChange={handleChange}/>
+      <Menu handleMenuClick={handleMenuClick}/>
+      {isContent === true ? <Content handleChange={handleChange}/>
+      : <p>Bruh</p>}
     <div>Pizza</div>
     </div>
   )
--- app/src/Menu.jsx
+++ app/src/Menu.jsx
@@ -1,17 +1,21 @@
+
 import content from './assets/content.png'
 import customize from './assets/customize.png'
-export default function Menu() {
-return(
-  <div className='menu'>
-    <button className='menu-btn'>
-      <img src={content} alt="" />
-      <p>Content</p>
-    </button>
 
-    <button className='menu-btn'>
-      <img src={customize} alt="" />
-      <p>Customize</p>
-    </button>
-  </div>
-)
+export default function Menu({ handleMenuClick }) {
+
+
+  return(
+    <div className='menu'>
+      <button className='menu-btn' id='content' onClick={handleMenuClick}>
+        <img src={content} alt="" />
+        <p>Content</p>
+      </button>
+
+      <button className='menu-btn' id='customize' onClick={handleMenuClick}>
+        <img src={customize} alt="" />
+        <p>Customize</p>
+      </button>
+    </div>
+  )
 }
