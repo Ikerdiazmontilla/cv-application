5c33d2d1efa971127cba51965054d5f7f26354fe
Ikerdiazmontilla <ikercorrevuela@gmail.com>
2024-05-13 09:16:09 -0500

    Style right and left layouts

--- app/src/cv/CV.jsx
+++ app/src/cv/CV.jsx
@@ -5,8 +5,10 @@ export default function CV({personalDetails, educationValues, experienceValues})
   return(
     <div className="cv top">
       <Header name={personalDetails.name} email={personalDetails.email} phone={personalDetails.phone} address={personalDetails.address}/>
-      <Information title={'Education'} sections={educationValues} isEducation={true}/>
-      <Information title={'Professional Experience'} sections={experienceValues}/>
+      <div className="info-wrapper">
+        <Information title={'Education'} sections={educationValues} isEducation={true}/>
+        <Information title={'Professional Experience'} sections={experienceValues}/>
+      </div>
     </div>
   )
 }
