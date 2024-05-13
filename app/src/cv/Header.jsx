0b2a50b634d130e72cb077f02861adab70952559
Ikerdiazmontilla <ikercorrevuela@gmail.com>
2024-05-13 09:31:37 -0500

    Add icons to header

--- app/src/cv/Header.jsx
+++ app/src/cv/Header.jsx
@@ -1,13 +1,14 @@
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faPhone, faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header({name , email, phone, address}) {
   return(
     <div className="header">
       <h1>{name}</h1>
       <div className="header-wrapper">
-        {email ? <div className="email">  {email}</div>: ""}
-        {phone ? <div className="phone">  {phone}</div>: ""}
-        {address ? <div className="address"> {address}</div>: ""}
+        {email ? <div className="email"><FontAwesomeIcon icon={faEnvelope} />  {email}</div>: ""}
+        {phone ? <div className="phone"><FontAwesomeIcon icon={faPhone} />  {phone}</div>: ""}
+        {address ? <div className="address"><FontAwesomeIcon icon={faLocationDot} /> {address}</div>: ""}
       </div>
     </div>
   )
