2292250061973d6465c90b0eeb866d9e52da97e6
Ikerdiazmontilla <ikercorrevuela@gmail.com>
2024-05-13 10:54:51 -0500

    Make content funnier

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -7,35 +7,54 @@ import { v4 as uuidv4 } from 'uuid';
 import TemplateLoader from './TemplateLoader';
 
 const initialPD = {
-  name: "Josephine Meyers",
-  email: "josephine.meyers@mail.co.uk",
+  name: "Average Worker",
+  email: "average.email@gmail.com",
   phone: "+44 3245 5521 5521",
-  address: "London, UK"
+  address: "Average Street, UK"
   }
 
 const initialEducationValues = {
   london: {
-    school: 'London City University',
-    degree: 'Bachelors in Economics',
+    school: 'Penguin University',
+    degree: 'Bachelors in penguin science',
     startDate: '08/2020',
     endDate: 'present',
-    location: 'New York City, US',
+    location: 'Penguinland, US',
     id: 'london',
     show:true,
     },
+  hidden: {
+    school: 'Hidden University',
+    degree: 'Masters degree in disappearing',
+    startDate: '08/2020',
+    endDate: 'present',
+    location: 'New York City, US',
+    id: 'hidden',
+    show:false,
+  }
 }
 
 const initialExperienceValues = {
   umbrella: {
     name: 'Umbrella Inc.',
-    position: 'UX & UI Designer',
+    position: 'Umbrella Designer',
     startDate: '08/2020',
     endDate: 'present',
-    location: 'New York City, US',
-    description: 'Designed and prototyped user interface patterns for various clients in various industries, ranging from self-service apps within the telecommunications-sector to mobile games for IOS and Android',
+    location: 'Raining City, US',
+    description: 'Designed and prototyped useful umbrellas for various clients (penguins) in various industries, ranging from barbie themed umbrellas within the princess-sector to lightning Mcqueen ones for cool people',
     id: 'umbrella',
     show: true,
     },
+    labs: {
+      name: 'Penguin Labs',
+      position: 'Penguin performance Research Assistant',
+      startDate: '04/2018',
+      endDate: '02/2019',
+      location: 'Top Secret, Germany',
+      description: 'Supported senior researchers on accessibility standards for the elder penguins. Created and tested best methods for athletic penguins to reach their peak vertical. Produced interactive documentation for quick onboarding of new researchers.',
+      id: 'labs',
+      show: true,
+      },
 }
 
 function App() {
