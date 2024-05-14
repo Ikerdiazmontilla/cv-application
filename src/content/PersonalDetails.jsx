044932e1b27a865227a78dbcbd266c909ab76b4d
Ikerdiazmontilla <ikercorrevuela@gmail.com>
2024-05-13 20:38:21 -0500

    Correct mismatch in PD input values

--- src/content/PersonalDetails.jsx
+++ src/content/PersonalDetails.jsx
@@ -7,10 +7,10 @@ export default function PersonalDetails({ handleChange }) {
     <form className="personal-details place">
       <h2>Personal Details</h2>
 
-      <Field id="name" handleChange={handleChange} label="Full Name" initValue="Josephine Meyers"/>
-      <Field id="email" handleChange={handleChange} label="Email" initValue="josephine.meyers@mail.co.uk"/>
+      <Field id="name" handleChange={handleChange} label="Full Name" initValue="Average Worker"/>
+      <Field id="email" handleChange={handleChange} label="Email" initValue="average.email@gmail.com"/>
       <Field id="phone" handleChange={handleChange} label="Phone Number" initValue="+44 3245 5521 5521"/>
-      <Field id="address" handleChange={handleChange} label="Address" initValue="London, UK"/>
+      <Field id="address" handleChange={handleChange} label="Address" initValue="Average Street, UK"/>
     </form>
   )
 }
